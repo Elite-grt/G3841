e0aca0b8bec57c3208c2cb55f13bd51cae879e96
Rinat <elite-grt@mail.ru>
2023-01-20 01:50:06 +0300

    add solution for task 0,1,3,5,7

---
+++ Sem1Task0/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача №0
+// Напишите программу, которая на вход принимает
+// число и выдаёт его квадрат (число умноженное на
+// само себя). 
+
+//Считаем данные с консоли 
+string? inputNum = Console.ReadLine();
+
+//Проверяем, чтобы данные были не пустые
+if(inputNum!=null)
+{
+
+      //Парсим введенное число
+      //int namber = int.Parse(inputNum);
+      //Находим квадрат числа
+      //int result = namber*namber;
+      //int result = (int)Math.Pow(namber,2);
+      
+      //Выводим данные в консоле
+      //Console.WriteLine(result);
+    Console.WriteLine(Math.Pow(int.Parse(inputNum),2));
+}
