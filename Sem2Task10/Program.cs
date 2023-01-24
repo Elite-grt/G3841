0c806c33b1c876fcb212fd35c7098058038ad8b5
Rinat <elite-grt@mail.ru>
2023-01-24 16:46:14 +0300

    add solution for task 10,13,15

---
+++ Sem2Task10/Program.cs
@@ -0,0 +1,7 @@
+﻿//Задача 10:
+// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int Namber = int.Parse(Console.ReadLine()??"0");
+int res = Namber/10;
+res = res%10;
+Console.WriteLine(res);
---
+++ Sem2Task13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+char[]digit = (Console.ReadLine()??"0").ToCharArray();
+
+
+if(digit.Length >= 3)
+{
+Console.WriteLine(digit[2]);
+// int num = int.Parse(Console.ReadLine()??"0");
+}
+
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
